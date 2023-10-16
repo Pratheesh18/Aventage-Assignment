89b1e5a3a5c854c225100d7b525e9429428fee0d
Pratheesh <pratheesh1218@gmail.com>
2023-10-16 16:40:41 +0530

    backend routes created

--- Question2/Backend/app.js
+++ Question2/Backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const patientRoutes = require('./routes/PatientRoute');
 require('dotenv').config();
 
 
@@ -10,10 +11,13 @@ mongoose.connect(process.env.MONGO_URL , {
     useUnifiedTopology : true
 }) .then(() => {
     console.log("Database Connected");
-})
+});
+
+app.use(express.json());
+app.use('/api/patients' , patientRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`);
 })
