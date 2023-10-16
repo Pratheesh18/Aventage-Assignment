e8b8ee4befa13ca669f7cfe712d33a02e50ff015
Pratheesh <pratheesh1218@gmail.com>
2023-10-16 16:37:20 +0530

    database connected

--- Question2/Backend/app.js
+++ Question2/Backend/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+
+mongoose.connect(process.env.MONGO_URL , {
+    useNewUrlParser : true , 
+    useUnifiedTopology : true
+}) .then(() => {
+    console.log("Database Connected");
+})
 
 
 const PORT = process.env.PORT || 3000;
---
+++ Question2/Backend/routes/PatientRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controllers/PatientController');
+
+
+router.post('/' , patientController.createPatient);
+router.get('/' , patientController.getAllPatients);
+router.get('/:patientId' , patientController.getOnePatient);
+router.put('/:patientId' , patientController.updatePatientDetails);
+router.delete('/:patientId' , patientController.deletePatient);
+
+
+module.exports = router;
